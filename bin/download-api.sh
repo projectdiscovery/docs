4782ec5f8038beb6428c053bd91b6a56687874cf
Brendan O'Leary <boleary@gitlab.com>
2023-12-06 17:11:01 -0500

    Refactor bin items

--- bin/download-api.sh
+++ bin/download-api.sh
@@ -2,6 +2,3 @@
 
 echo "Downloading file"
 wget -O openapi.yaml https://stoplight.io/api/v1/projects/projectdiscovery/pdcp-api-new/nodes/openapi.yaml\?fromExportButton\=true\&snapshotType\=http_service
-
-echo "Generating from Mintlify scraping"
-npx @mintlify/scraping@latest openapi-file openapi.yaml -o api-reference
---
+++ bin/generate-api.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "Generating from Mintlify scraping"
+npx @mintlify/scraping@latest openapi-file openapi.yaml -o api-reference
